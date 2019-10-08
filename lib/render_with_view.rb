ec18a543a28545f7eed36eb9a7507f254fa99cba
Mikkel Malmberg <mikkel@brnbw.com>
2019-10-08 13:10:41 +0200

    Avoid unnecessary module gymnastics

--- lib/render_with_view.rb
+++ lib/render_with_view.rb
@@ -2,25 +2,22 @@ require 'render_with_view/version'
 require 'render_with_view/half_open_struct'
 
 module RenderWithView
-  class View < HalfOpenStruct; end
+  class View < HalfOpenStruct
+  end
 
-  def self.included kls
-    kls.class_eval do
-      def render_with_view *args
-        if !args.first.is_a?(Hash)
-          tmpl = args.shift
-        else
-          tmpl = action_name
-        end
-        locals = args.shift
-        opts = args.shift || {}
-
-        # save to ivar for testing purposes
-        @__view__ = View.new(locals)
-
-        render tmpl, opts.merge(locals: { view: @__view__ })
-      end
+  def render_with_view *args
+    if !args.first.is_a?(Hash)
+      tmpl = args.shift
+    else
+      tmpl = action_name
     end
+    locals = args.shift
+    opts = args.shift || {}
+
+    # save to ivar for testing purposes
+    @__view__ = View.new(locals)
+
+    render tmpl, opts.merge(locals: { view: @__view__ })
   end
 end
 
